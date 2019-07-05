bfb7679120bba83c28a6870bc682eb49e36d6fd4
ThoMaCarto <49810626+ThoMaCarto@users.noreply.github.com>
2019-07-05 17:26:52 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -11,8 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://thomacarto.github.io/AtlasUECI/public/'
-RELATIVE_URLS = True
+SITEURL = 'https://thomacarto.github.io/atlasUECI/'
+RELATIVE_URLS = False
 
 #FEED_ALL_ATOM = 'feeds/all.atom.xml'
 #CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
