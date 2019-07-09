3eec8ee1497bcde2bf179549bb5559ad11ab7256
ThoMaCarto <thomas.maillard.p8@gmail.com>
2019-07-09 20:14:32 +0200

    amélioration page diagnostic localité

--- content/doc/map/localites_fiche.js
+++ content/doc/map/localites_fiche.js
@@ -3,8 +3,7 @@ $.getJSON(urllocalites,function(data)
 {
 	var localites= L.geoJson(data,{style: function(feature){
 		return { color : 'red', weight : 1.5, fillColor : 'red', fillOpacity : .0, };},
-		onEachFeature : function(feature, layer ) {layer.bindPopup('<b><u>Nom de la localité</u></b><br>' + feature.properties.a_quartier +'<br>Niveau de Vulnérabilité : '+ feature.properties.vulnerabil+
-	 '<br><strong> Diagnostic détaillé : </strong> <a href="articles/' + articlespath + feature.properties.lien +'" style="text-transform: capitalize;">'+ feature.properties.a_quartier +'</a>')}
+		onEachFeature: oneachfeature,
 });
 //localites.addTo(map);
 controlLayers.addOverlay(localites, "Localités","Diagnostic des localités");
@@ -18,6 +17,65 @@ div.innerHTML ='<strong>Niveau de vulnérabilité des localités</strong><table 
 return div;
 }
 maptitle.addTo(map);
+
+ // fonction pour l'affichage dans le panneau à droite des données du point       
+function oneachfeature(feature, layer){
+		 	//marker.setOpacity(0); 
+           layer.on({
+                click: function showResultsInDiv() {
+                    var d = document.getElementById('popupstatic');
+                    d.innerHTML = "";
+                    tableNew = '<table>';
+                    //tableNew += "<tr><td></td></tr>";
+                       
+                        for (prop in feature.properties){
+                        	
+                        	
+                        	 if (feature.properties[prop] !== 'nd' 
+                        	 && feature.properties[prop] !== null 
+                        	 && prop !== 'slug' 
+                        	 && prop !== 'field_36' 
+                        	 && prop !== 'link'
+                        	 && prop !== 'link_1'
+                        	 && prop !== 'row id'
+                        	 && prop !== 'a_préfect'
+                        	 && prop !== 'a__sous_pr'
+                        	 && prop !== 'a_sous_qua'
+                        	 && prop !== 'id') 
+                        		{
+                        			tableNew += '<tr><td style="max-width:300px;"><strong>'+prop+" : </strong></td><td>"+feature.properties[prop]+"</td></tr>" ;
+                        			}
+                        		else {;}
+                        		;
+                        		
+                        	  }                     	
+                        	
+                        
+                    
+                    tableNew += "</table>";
+                    d.innerHTML = tableNew;
+                    
+                    
+                }
+            }) }
+//fonction pour afficher un cible bleu autour du point sélectionné
+function cible(e){
+						
+		var marker = new L.circleMarker([e.latlng.lat, e.latlng.lng],{radius: 10, color: 'dodgerblue', fillColor: 'dodgerblue',weight: 3,fillOpacity: 0,dashArray:"5",})
+		.addTo(map);
+		if (markers.length > 0) {map.removeLayer(markers.pop());}
+		var marker;
+		markers.push(marker)
+      return marker;
+		
+		
+}
+
+//Création d'un compteur de marker
+var markers = [];	   
+
+
+
 
 ////Fonction qui défini la couleur en fonction de la vulnérabilité  
 /// e sera le nombre de point d'eau conforme, f le nmbre de pts d'eau diag, et g le raccordement à la SODECI, h le nombre de pompe fonctionnelles        
@@ -47,12 +105,15 @@ function getColorVuln (e,f,g,h){
 	}
 }; 
  
+// fonction d'affichage de la DB
+
+
 
 // création d’une couche geoJson qui appelle le fichier « localites.geojson »
 $.getJSON(urllocalites,function(data2)
 {
 	var vulnLocalites= L.geoJson(data2,{style: function(feature){return { color : getColorVuln(feature.properties.Points_deau_diagnostiqués_conformes_aux_normes_OMS,feature.properties.Nombre_de_points_deau_diagnostiqués,feature.properties.EAU_SODECI,feature.properties.Pompes_Fonctionnelles), weight : 1, fillColor : getColorVuln(feature.properties.Points_deau_diagnostiqués_conformes_aux_normes_OMS,feature.properties.Nombre_de_points_deau_diagnostiqués,feature.properties.EAU_SODECI,feature.properties.Pompes_Fonctionnelles), fillOpacity : .5 };},
-	onEachFeature : function(feature, layer ) {layer.bindPopup('<b><u>Nom de la localité</u></b><br>' + feature.properties.a_quartier +'<br><strong> Diagnostic détaillé : </strong> <a href="articles/' + articlespath + feature.properties.lien +'" style="text-transform: capitalize;">'+ feature.properties.a_quartier +'</a>')}
+	onEachFeature: oneachfeature,
 });
 vulnLocalites.addTo(map);
 controlLayers.addOverlay(vulnLocalites, "Niveau de Vulnérabilité","Diagnostic des localités");
---
+++ output/doc/map/pe_type.js
@@ -0,0 +1,162 @@
+//
+
+
+////Création de la couche (Layer) type de point d'eau
+//Couleur en fonction du type de point d'eau
+function getColortype(Type_de_point_deau) {
+          switch (Type_de_point_deau) {
+            case 'puits_ouvert':
+              return  'blue';
+            case 'point_equipe':
+              return 'purple';
+            case 'surface':
+              return 'green';
+     		default:
+              return 'grey';
+          }
+        };
+        
+      
+//transformation de la base de donné "point_eau.geojson" en une couche type de points d'eau
+//lecture de la base de donnée
+$.getJSON(urlpointdeau,function(data){
+	//transformation de la base de donnée en couche (Layer)
+	var typePE = L.geoJson(data,{
+		//Affichage sous forme de point
+		pointToLayer:function(feature,latlng){
+			//caractéristique des points
+			var marker = L.circleMarker(latlng,{radius: 5,fillOpacity: 1, color: 'black', fillColor: getColortype(feature.properties.Type_de_point_deau),weight: 1,fillOpacity: 0.8,});
+			//caractéristiques des popup
+			marker.bindPopup('<b><u>Type de point d\'eau</u></b><br><i>' + feature.properties.Type_de_point_deau
+												+ ' <br><i>détail:</i> ' + feature.properties.B_type + '<br><b><u>Nom du point d\'eau : </u></b>'+feature.properties.A_nom
+												);
+			//Affichage des marqueurs
+			return marker;
+		}
+		});
+	//Affichage sur la carte
+	//typePE.addTo(map);
+	//Affichage dans le controleur de couche
+	//controlLayers.addOverlay(typePE, "Type de point d'eau");
+	//controlLayers.addOverlay(typePE, "Type de point d'eau", "Diagnostic des points d'eau");
+});
+
+//Création de la couche contamination Ecoli
+//Couleur en fonction de la présence d'E. coli
+
+  function getColorEcoli(ecoli) {
+          if (ecoli == 0 == true) {
+          	return('green');
+          }
+          else if (ecoli > 0 == true) {
+          	return('Red');
+          }
+          else {
+          	return('grey');
+          }
+       };
+       
+//transformation de la base de donné "point_eau.geojson" en une couche E.coli
+//lecture de la base de donnée
+$.getJSON(urlpointdeau,function(data){
+	//transformation de la base de donnée en couche (Layer)
+	
+	var Ecoli = L.geoJson(data,{
+		//Affichage sous forme de point
+		pointToLayer:function(feature,latlng){
+			//caractéristique des points
+			var marker2 = L.circleMarker(latlng,{radius: 5,fillOpacity: 1, color: 'black', fillColor: getColorEcoli(feature.properties.ecoli),weight: 1,fillOpacity: 0.8,});
+			//caractéristiques des popup
+			marker2.bindPopup('<p>Comptage <em>E. coli</em>: ' + feature.properties.ecoli + ' UFC / 100 ml</p>');
+			//Affichage des marqueurs
+			return marker2;
+		}
+		});
+	//Affichage sur la carte
+	
+	Ecoli.addTo(map);
+	//Affichage dans le controleur de couche
+	
+	//controlLayers.addOverlay(Ecoli, "" );
+	controlLayers.addOverlay(Ecoli, "Contamination par E. coli", "Diagnostic des points d'eau");
+});
+
+
+//Création de la couche pollution nitrates
+//Couleur en fonction de la concentration en Nitrates
+
+  function getColorNitrates(Nitrate) {
+          if (Nitrate < 50 == true) {
+          	return('green');
+          }
+          else if (Nitrate >= 50 == true) {
+          	return('Red');
+          }
+          else {
+          	return('grey');
+          }
+       };     
+//transformation de la base de donné "point_eau.geojson" en une couche nitrate
+//lecture de la base de donnée
+$.getJSON(urlpointdeau,function(data){
+	//transformation de la base de donnée en couche (Layer)
+	
+	var Nitrate = L.geoJson(data,{
+		//Affichage sous forme de point
+		pointToLayer:function(feature,latlng){
+			//caractéristique des points
+			var marker = L.circleMarker(latlng,{radius: 5,fillOpacity: 1, color: 'black', fillColor: getColorNitrates(feature.properties.nitrate),weight: 1,fillOpacity: 0.8,});
+			//caractéristiques des popup
+			marker.bindPopup(' <p>Concentration en <em>Nitrate</em>: ' + feature.properties.nitrate + ' mg/l </p>');
+			//Affichage des marqueurs
+			return marker;
+		}
+		});
+	//Affichage sur la carte
+	
+	
+	//controlLayers.addOverlay(Ecoli, "" );
+	controlLayers.addOverlay(Nitrate, "Pollution au Nitrate", "Diagnostic des points d'eau");
+});
+
+//Création de la couche pollution Ammoniac
+//Couleur en fonction de la concentration en Ammoniac
+
+  function getColorAmmoniac(Ammoniac) {
+          if (Ammoniac < 1 == true) {
+          	return('green');
+          }
+          else if (Ammoniac >= 1 == true) {
+          	return('Red');
+          }
+          else {
+          	return('grey');
+          }
+       };     
+//transformation de la base de donné "point_eau.geojson" en une couche Ammoniac
+//lecture de la base de donnée
+$.getJSON(urlpointdeau,function(data){
+	//transformation de la base de donnée en couche (Layer)
+	
+	var Ammoniac = L.geoJson(data,{
+		//Affichage sous forme de point
+		pointToLayer:function(feature,latlng){
+			//caractéristique des points
+			var marker = L.circleMarker(latlng,{radius: 5,fillOpacity: 1, color: 'black', fillColor: getColorAmmoniac(feature.properties.ammoniac),weight: 1,fillOpacity: 0.8,});
+			//caractéristiques des popup
+			marker.bindPopup(' <p>Concentration en <em>Ammoniac</em>: ' + feature.properties.ammoniac + ' mg/l </p>');
+			//Affichage des marqueurs
+			return marker;
+		}
+		});
+	//Affichage sur la carte
+	
+	
+	
+	controlLayers.addOverlay(Ammoniac, "Pollution à l'Ammoniac", "Diagnostic des points d'eau");
+});
+
+//Création d'une couche avec un icone
+
+
+
