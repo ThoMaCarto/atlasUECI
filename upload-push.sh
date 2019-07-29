7081a23cd799e5508915f32d1c9c146628f9f5db
ThoMaCarto <thomas.maillard.p8@gmail.com>
2019-07-29 15:03:09 +0200

    mise à jour village

---
+++ upload-push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /home/tmaillard/site_internet/protoUECI
+
+source venv/bin/activate
+
+read -p 'saisir le commentaire du commit:' commentaire
+
+pelican content -s publishconf.py && pelican content -s atlasconf.py
+
+ git pull https://github.com/ThoMaCarto/atlasUECI.git
+
+git status
+
+git add --all
+
+git commit -m "$commentaire"
+
+git status
+
+git push https://github.com/ThoMaCarto/atlasUECI.git master
