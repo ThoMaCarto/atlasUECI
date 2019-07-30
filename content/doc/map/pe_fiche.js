7dfeb2aa0d7a4b2f6d303dc3d43a14f72265da89
ThoMaCarto <49810626+ThoMaCarto@users.noreply.github.com>
2019-07-30 21:17:51 +0200

    Update pe_fiche.js

--- content/doc/map/pe_fiche.js
+++ content/doc/map/pe_fiche.js
@@ -181,22 +181,42 @@ function getColorAmmoniac(Ammoniac) {
           }
        };  
        
-          
+ // Couleur en fonction de la norme OMS
+
+function getLayerOMS (Name,Field,minOMS,maxOMS)
+{
+	function getColorOMS (Field,minOMS,maxOMS){
+	if (Field < minOMS == true) {return('red');}
+	else if (Field <= maxOMS == true) {return('green');}
+	else if (Field > maxOMS == true) {return('red');}
+	else  {return('grey');}
+	};
+	
+	var LayerOMS = L.geoJson(data,{
+		onEachFeature : oneachfeature,
+		pointToLayer: function(feature,latlng){
+			var marker =  L.circleMarker(latlng,{radius: 5,fillOpacity: 1, color: 'black', fillColor: getColorOMS,weight: 1,fillOpacity: 0.8,});
+			return marker;
+		}
+	}).on("click", cible)
+	.addTo(map);
+	controlLayers.addOverlay(LayerOMS, 'Contamination par'+ Name, '<strong>Diagnostic des points d\'eau</strong>');
+}; 
 
 //lecture de la base de donnée
 $.getJSON(urlpointdeau,function(data){
 	
 	//transformation de la base de donnée en couche (Layer)
-	
+	getLayerOMS(nitrite,feature.properties.nitrite,0,0.1);
 	// création de la couche E coli
 	var Ecoli = L.geoJson(data,{
 		onEachFeature: oneachfeature,
 		//Affichage sous forme de point
 		pointToLayer:function(feature,latlng){
 			//caractéristique des points
-			var marker2 = L.circleMarker(latlng,{radius: 5,fillOpacity: 1, color: 'black', fillColor: getColorEcoli(feature.properties.ecoli),weight: 1,fillOpacity: 0.8,});
+			var marker = L.circleMarker(latlng,{radius: 5,fillOpacity: 1, color: 'black', fillColor: getColorEcoli(feature.properties.ecoli),weight: 1,fillOpacity: 0.8,});
 			//Affichage des marqueurs
-			return marker2;
+			return marker;
 		}
 		}).on("click", cible);
 	//Affichage sur la carte
